8ea83ad79d0115b5e63bdb25ab739c6b639c813f
Gaobaiawa <15157827117@163.com>
2023-10-15 03:03:21 +0800

    Initial commit

--- Plain Craft Launcher 2/Controls/IMyRadio.vb
+++ Plain Craft Launcher 2/Controls/IMyRadio.vb
@@ -1,4 +1,8 @@
-﻿Public Interface IMyRadio
+﻿' 这是一个公共接口用于描述一个自定义的广播器（机翻，我只负责混pr
+Public Interface IMyRadio
+    ' 当选中状态发生变化时触发的事件
     Event Check(sender As Object, e As RouteEventArgs)
+
+    ' 当广播器状态发生变化时触发的事件
     Event Changed(sender As Object, e As RouteEventArgs)
 End Interface
