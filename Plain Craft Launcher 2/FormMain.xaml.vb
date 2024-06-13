937f034a1b9155f489e7df41f2f6b539606f06cf
Pigeon0v0 <PigeonA0v0@outlook.com>
2024-06-13 22:23:36 +0800

    feat: 支持下载资源包

--- Plain Craft Launcher 2/FormMain.xaml.vb
+++ Plain Craft Launcher 2/FormMain.xaml.vb
@@ -1023,6 +1023,7 @@ Install:
         DownloadLiteLoader = 8
         DownloadMod = 10
         DownloadPack = 11
+        DownloadResourcePack = 12
         SetupLaunch = 0
         SetupUI = 1
         SetupSystem = 2
@@ -1058,8 +1059,10 @@ Install:
                         Return "Mod 下载 - " & Project.TranslatedName
                     Case CompType.ModPack
                         Return "整合包下载 - " & Project.TranslatedName
-                    Case Else 'CompType.ResourcePack
+                    Case CompType.ResourcePack
                         Return "资源包下载 - " & Project.TranslatedName
+                    Case Else 'CompType.ShaderPack
+                        Return "光影包下载 - " & Project.TranslatedName
                 End Select
             Case PageType.HelpDetail
                 Dim Entry As HelpEntry = Stack.Additional(0)
