80f236026954391a2d24daf1228ce2bcc7349903
tangge233 <2697992791@qq.com>
2024-07-15 12:46:07 +0800

    lang: 提前语言设置加载以应用更多文本的国际化

--- Plain Craft Launcher 2/FormMain.xaml.vb
+++ Plain Craft Launcher 2/FormMain.xaml.vb
@@ -333,30 +333,6 @@ Public Class FormMain
             '触发降级
             DowngradeSub(LastVersion)
         End If
-        '刷新语言
-        Lang = ReadReg("Lang")
-        If String.IsNullOrWhiteSpace(Lang) Then
-            Select Case Globalization.CultureInfo.CurrentCulture.Name
-                Case "en-US"
-                    Lang = "en_US"
-                Case "zh-CN"
-                    Lang = "zh_CN"
-                Case "zh-HK"
-                    Lang = "zh_HK"
-                Case "zh-TW"
-                    Lang = "zh_TW"
-                Case Else
-                    Lang = "en_US"
-            End Select
-            WriteReg("Lang", Lang)
-        End If
-        Log("[Lang] 选择启动器语言为 " & Lang)
-        Try
-            Application.Current.Resources.MergedDictionaries(1) = New ResourceDictionary With {.Source = New Uri("pack://application:,,,/Resources/Language/" & Lang & ".xaml", UriKind.RelativeOrAbsolute)}
-        Catch ex As Exception
-            Log("无法找到语言资源：" & Lang & vbCrLf & "Language resource cannot be found:" & Lang, LogLevel.Assert)
-        End Try
-
         '刷新主题
         ThemeCheckAll(False)
         Setup.Load("UiLauncherTheme")
