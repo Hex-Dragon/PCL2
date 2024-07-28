5633a0225d30cf640592284154b46543c90cc4f0
DanKe <1586017466@QQ.com>
2024-07-28 23:28:51 +0800

    添加CurseForge API Key

---
+++ CurseForge API Key.md
@@ -0,0 +1,9 @@
+# CurseForge API Key
+
+### 来源：[cf.polymc.org/api](https://cf.polymc.org/api)
+
+$2a$10$bL4bIL5pUWqfcO7KQtnMReakwtfHbNKh6v1uTpKlzhwoueEJQnPnm
+
+### 来源：[HMCL Project](https://github.com/HMCL-dev/HMCL)
+
+$2a$10$o8pygPrhvKBHuuh5imL2W.LCNFhB15zBYAExXx/TqTx/Zp5px2lxu
--- Plain Craft Launcher 2/Modules/ModSecret.vb
+++ Plain Craft Launcher 2/Modules/ModSecret.vb
@@ -104,7 +104,7 @@ Friend Module ModSecret
             Client.Headers("User-Agent") = "PCL2/" & VersionStandardCode
         End If
         Client.Headers("Referer") = "http://" & VersionCode & ".pcl2.server/"
-        '如果你有 CurseForge API Key，请添加到 Headers 中，以恢复对 CurseForge 的访问
+        Client.Headers("x-api-Key") = "$2a$10$bL4bIL5pUWqfcO7KQtnMReakwtfHbNKh6v1uTpKlzhwoueEJQnPnm" 'CurseForge API Key
     End Sub
     ''' <summary>
     ''' 设置 Headers 的 UA、Referer。
@@ -117,7 +117,7 @@ Friend Module ModSecret
             Request.UserAgent = "PCL2/" & VersionStandardCode
         End If
         Request.Referer = "http://" & VersionCode & ".pcl2.server/"
-        '如果你有 CurseForge API Key，请添加到 Headers 中，以恢复对 CurseForge 的访问
+        Request.Headers("x-api-Key") = "$2a$10$bL4bIL5pUWqfcO7KQtnMReakwtfHbNKh6v1uTpKlzhwoueEJQnPnm" 'CurseForge API Key
     End Sub
 
 #End Region
