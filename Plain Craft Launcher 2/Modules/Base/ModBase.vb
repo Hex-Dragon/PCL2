4d28be26104b131633284523672bc911c9e1e0b1
allMagic <116283787+allMagicNB@users.noreply.github.com>
2024-08-31 17:55:06 +0800

    更新 ModBase.vb

--- Plain Craft Launcher 2/Modules/Base/ModBase.vb
+++ Plain Craft Launcher 2/Modules/Base/ModBase.vb
@@ -11,8 +11,8 @@ Public Module ModBase
 #Region "声明"
 
     '下列版本信息由更新器自动修改
-    Public Const VersionBaseName As String = "2.8.4" '不含分支前缀的显示用版本名
-    Public Const VersionStandardCode As String = "2.8.4." & VersionBranchCode '标准格式的四段式版本号
+    Public Const VersionBaseName As String = "2.8.5" '不含分支前缀的显示用版本名
+    Public Const VersionStandardCode As String = "2.8.5." & VersionBranchCode '标准格式的四段式版本号
     'CommitHash
     Public Const CommitHash As String = ""
 #If BETA Then
