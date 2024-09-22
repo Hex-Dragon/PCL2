070c24482a0a6da30543a0f3fa052111f9a7d0c6
Ad-closeNN <1709301095@qq.com>
2024-09-22 13:02:38 +0800

    更改链接
    原链接失效

--- Plain Craft Launcher 2/Pages/PageSetup/ModSetup.vb
+++ Plain Craft Launcher 2/Pages/PageSetup/ModSetup.vb
@@ -541,7 +541,7 @@
                 FrmSetupUI.HintCustom.Visibility = Visibility.Visible
                 FrmSetupUI.HintCustom.Text = $"从指定网址联网获取主页内容。服主也可以用于动态更新服务器公告。{vbCrLf}如果你制作了稳定运行的联网主页，可以点击这条提示投稿，若合格即可加入预设！"
                 FrmSetupUI.HintCustom.EventType = "打开网页"
-                FrmSetupUI.HintCustom.EventData = "https://github.com/Hex-Dragon/PCL2/discussions/2528"
+                FrmSetupUI.HintCustom.EventData = "https://github.com/Hex-Dragon/PCL2/discussions/new?category=%E8%87%AA%E5%AE%9A%E4%B9%89%E4%B8%BB%E9%A1%B5"
             Case 3 '预设
                 FrmSetupUI.PanCustomPreset.Visibility = Visibility.Visible
                 FrmSetupUI.PanCustomLocal.Visibility = Visibility.Collapsed
