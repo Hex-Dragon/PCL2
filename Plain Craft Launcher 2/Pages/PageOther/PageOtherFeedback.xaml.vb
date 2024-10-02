82009ebfc3af9019ac700427d83f4b1849fc0e3e
tangge233 <tangge233@qq.com>
2024-10-02 21:05:02 +0800

    feat: 反馈具体化页面

--- Plain Craft Launcher 2/Pages/PageOther/PageOtherFeedback.xaml.vb
+++ Plain Craft Launcher 2/Pages/PageOther/PageOtherFeedback.xaml.vb
@@ -5,6 +5,7 @@
 
         '重复加载部分
         PanBack.ScrollToHome()
+        RefreshList()
 
         '非重复加载部分
         If IsLoaded Then Exit Sub
@@ -13,4 +14,26 @@
 
     End Sub
 
+    Public Sub RefreshList()
+        PanContent.Children.Clear()
+        PanContent.Children.Add(New MyLoading() With {.Text = "加载反馈列表中……", .HorizontalAlignment = HorizontalAlignment.Center})
+        Dim list As JArray
+        Dim thread As Thread = RunInNewThread(Sub()
+                                                  list = NetGetCodeByRequestRetry("https://api.github.com/repos/Hex-Dragon/PCL2/issues", IsJson:=True, UseBrowserUserAgent:=True)
+                                              End Sub, "GetFeedbackList")
+        While thread.IsAlive
+
+        End While
+        PanContent.Children.Clear()
+        If List Is Nothing Then Exit Sub
+
+        For Each i As JObject In list
+            Dim item As MyListItem = New MyListItem With {.Title = i("title").ToString(), .Info = "用户：" & i("user")("login").ToString() & "；时间：" & Date.Parse(i("created_at").ToString()).ToLocalTime().ToString()}
+            PanContent.Children.Add(item)
+        Next
+    End Sub
+
+    Private Sub Feedback_Click(sender As Object, e As MouseButtonEventArgs)
+        PageOtherLeft.TryFeedback()
+    End Sub
 End Class
