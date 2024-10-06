b689bf5b50b1c382b65342d818a568bfa0c62138
tangge233 <tangge233@qq.com>
2024-10-06 13:56:12 +0800

    feat: use loader

--- Plain Craft Launcher 2/Pages/PageOther/PageOtherFeedback.xaml.vb
+++ Plain Craft Launcher 2/Pages/PageOther/PageOtherFeedback.xaml.vb
@@ -1,46 +1,53 @@
 ﻿Public Class PageOtherFeedback
 
+    Public Class Feedback
+        Public User As String
+        Public Title As String
+        Public Time As String
+        Public Content As String
+        Public Url As String
+    End Class
+
     Private Shadows IsLoaded As Boolean = False
     Private Sub PageOtherFeedback_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
-
+        PageLoaderInit(Load, PanLoad, PanContent, PanInfo, Loader, AddressOf RefreshList, AddressOf LoaderInput)
         '重复加载部分
         PanBack.ScrollToHome()
-
         '非重复加载部分
         If IsLoaded Then Exit Sub
         IsLoaded = True
 
-        RefreshList()
+    End Sub
 
+    Public Shared Loader As New LoaderTask(Of String, List(Of Feedback))("FeedbackList", AddressOf FeedbackListGet, AddressOf LoaderInput) With {.ReloadTimeout = 60 * 1000}
+
+    Private Shared Function LoaderInput() As String
+        Return "" ' awa?
+    End Function
+
+    Public Shared Sub FeedbackListGet(Task As LoaderTask(Of String, List(Of Feedback)))
+        Dim list As JArray
+        list = NetGetCodeByRequestRetry("https://api.github.com/repos/Hex-Dragon/PCL2/issues", IsJson:=True, UseBrowserUserAgent:=True)
+        If list Is Nothing Then Throw New Exception("无法获取到内容")
+        Dim res As List(Of Feedback) = New List(Of Feedback)
+        For Each i As JObject In list
+            Dim item As Feedback = New Feedback With {.Title = i("title").ToString(), .Url = i("html_url").ToString(), .Content = i("body").ToString(), .Time = Date.Parse(i("created_at").ToString()).ToLocalTime().ToString(), .User = i("user")("login").ToString()}
+            res.Add(item)
+        Next
+        Task.Output = res
     End Sub
 
     Public Sub RefreshList()
-        PanContent.Children.Clear()
-        PanContent.Children.Add(New MyLoading() With {.Text = "加载反馈列表中……", .HorizontalAlignment = HorizontalAlignment.Center})
-        Dim list As JArray
-        Dim thread As Thread = RunInNewThread(Sub()
-                                                  list = NetGetCodeByRequestRetry("https://api.github.com/repos/Hex-Dragon/PCL2/issues", IsJson:=True, UseBrowserUserAgent:=True)
-                                              End Sub, "GetFeedbackList")
-        While thread.IsAlive
-            Thread.Sleep(100)
-        End While
-        PanContent.Children.Clear()
-        If List Is Nothing Then Exit Sub
-
-        For Each i As JObject In list
-            Dim item As MyListItem = New MyListItem With {.Title = i("title").ToString(), .Info = "反馈者：" & i("user")("login").ToString() & " | 时间：" & Date.Parse(i("created_at").ToString()).ToLocalTime().ToString(), .Logo = $"https://github.com/{i("user")("login").ToString()}.png", .Type = MyListItem.CheckType.Clickable, .Tag = i}
-            AddHandler item.Click, AddressOf SeeDetail
-            PanContent.Children.Add(item)
+        For Each item In Loader.Output
+            Dim ele As New MyListItem With {.Title = item.Title, .Info = item.User & " | " & item.Time, .Logo = $"https://github.com/{item.User}.png", .Type = MyListItem.CheckType.Clickable}
+            AddHandler ele.Click, Sub()
+                                      MyMsgBox(item.Content, item.User & " | " & item.Title, Button2:="查看详情", Button2Action:=Sub()
+                                                                                                                                 OpenWebsite(item.Url)
+                                                                                                                             End Sub)
+                                  End Sub
+            PanList.Children.Add(ele)
         Next
     End Sub
-
-    Private Sub SeeDetail(sender As Object, e As MouseButtonEventArgs)
-        Dim data As JObject = sender.Tag
-        MyMsgBox(data("body").ToString(), data("user")("login").ToString() & " | " & data("title").ToString(), Button2:="查看详情", Button2Action:=Sub()
-                                                                                                                                                   OpenWebsite(data("html_url"))
-                                                                                                                                               End Sub)
-    End Sub
-
 
     Private Sub Feedback_Click(sender As Object, e As MouseButtonEventArgs)
         PageOtherLeft.TryFeedback()
--- Plain Craft Launcher 2/Pages/PageOther/PageOtherVote.xaml.vb
+++ Plain Craft Launcher 2/Pages/PageOther/PageOtherVote.xaml.vb
@@ -5,60 +5,55 @@ Imports System.Windows.Forms.LinkLabel
 Imports System.Xml
 
 Public Class PageOtherVote
+    Public Class Vote
+        Public Title As String
+        Public Url As String
+    End Class
 
     Private Shadows IsLoaded As Boolean = False
     Private Sub PageOtherFeedback_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
-
+        PageLoaderInit(Load, PanLoad, PanContent, PanInfo, Loader, AddressOf LoadList, AddressOf LoaderInput)
         '重复加载部分
         PanBack.ScrollToHome()
 
         '非重复加载部分
         If IsLoaded Then Exit Sub
         IsLoaded = True
-        LoadList()
 
     End Sub
+    Public Shared Loader As New LoaderTask(Of String, List(Of Vote))("VoteList", AddressOf VoteListGet, AddressOf LoaderInput) With {.ReloadTimeout = 60 * 1000}
 
-    Public Sub LoadList()
-        PanContent.Children.Clear()
-        PanContent.Children.Add(New MyLoading() With {.Text = "加载投票列表中……", .HorizontalAlignment = HorizontalAlignment.Center})
-        Dim content As String
-        Dim thread As Thread = RunInNewThread(Sub()
-                                                  content = NetGetCodeByRequestRetry("https://github.com/Hex-Dragon/PCL2/discussions/categories/%E5%8A%9F%E8%83%BD%E6%8A%95%E7%A5%A8?discussions_q=is%3Aopen+category%3A%E5%8A%9F%E8%83%BD%E6%8A%95%E7%A5%A8+sort%3Atop")
-                                              End Sub, "GetVoteList")
-        While thread.IsAlive
-            Thread.Sleep(100)
-        End While
+    Private Shared Function LoaderInput() As String
+        Return "" ' awa?
+    End Function
 
-        PanContent.Children.Clear()
+    Public Shared Sub VoteListGet(Task As LoaderTask(Of String, List(Of Vote)))
+        Dim content = NetGetCodeByRequestRetry("https://github.com/Hex-Dragon/PCL2/discussions/categories/%E5%8A%9F%E8%83%BD%E6%8A%95%E7%A5%A8?discussions_q=is%3Aopen+category%3A%E5%8A%9F%E8%83%BD%E6%8A%95%E7%A5%A8+sort%3Atop")
         If content Is Nothing Then Exit Sub
         Dim regexPattern As String = "<a[^>]*?\bdata-hovercard-type=""discussion""[^>]*?href=""(.*?)""[^>]*?>(.*?)</a>"
 
+        Dim res As List(Of Vote) = New List(Of Vote)
+
         ' 使用正则表达式匹配
         Dim matches As MatchCollection = Regex.Matches(content, regexPattern, RegexOptions.Singleline)
-
         For Each match As Match In matches
             If match.Success Then
                 ' 输出匹配到的href和文本内容
-                Dim item As MyListItem = New MyListItem With {.Title = match.Groups(2).Value.Trim(), .Logo = "https://github.githubassets.com/assets/1fa9c-fd94340a7b39.png", .Type = MyListItem.CheckType.Clickable}
-                AddHandler item.Click, Sub()
-                                           OpenWebsite("https://github.com/" & match.Groups(1).Value)
-                                       End Sub
-                PanContent.Children.Add(item)
+                Dim item As Vote = New Vote With {.Title = match.Groups(2).Value.Trim(), .Url = "https://github.com/" & match.Groups(1).Value}
+                res.Add(item)
             End If
         Next
-        Exit Sub
-        Dim doc As New XmlDocument()
-        ' 加载XML内容
-        doc.LoadXml(content)
-        ' 获取所有a元素
-        Dim links As XmlNodeList = doc.SelectNodes("//a")
-        ' 遍历所有a元素
-        For Each link As XmlNode In links
-            If link.Attributes("data-hovercard-type").Value.Equals("discussion") Then
-                Dim item As MyListItem = New MyListItem With {.Title = link.InnerText}
-                PanContent.Children.Add(item)
-            End If
+        Task.Output = res
+    End Sub
+
+
+    Public Sub LoadList()
+        For Each item In Loader.Output
+            Dim ele As MyListItem = New MyListItem With {.Logo = "https://github.githubassets.com/assets/1fa9c-fd94340a7b39.png", .Type = MyListItem.CheckType.Clickable, .Title = item.Title}
+            AddHandler ele.Click, Sub()
+                                      OpenWebsite(item.Url)
+                                  End Sub
+            PanList.Children.Add(ele)
         Next
     End Sub
 
