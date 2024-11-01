07ac34dafe98ca1a25efa30fa33bbbbc83c5195e
ZeroWolf233 <z@l0.ink>
2024-11-01 21:37:24 +0800

    chore: 删掉莫名其妙的第7行

--- Plain Craft Launcher 2/Pages/PageLaunch/PageLaunchRight.xaml.vb
+++ Plain Craft Launcher 2/Pages/PageLaunch/PageLaunchRight.xaml.vb
@@ -4,7 +4,6 @@
         PanBack.ScrollToHome()
         PanScroll = PanBack '不知道为啥不能在 XAML 设置
         PanLog.Visibility = If(ModeDebug, Visibility.Visible, Visibility.Collapsed)
-        ' 
         AddHandler Me.KeyDown, AddressOf PressF5
         '快照版提示
 #If BETA Then
