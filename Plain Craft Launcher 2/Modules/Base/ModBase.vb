1d089a6697b026ca392781086731d90755bb587a
ZeroWolf233 <z@l0.ink>
2024-11-08 21:56:00 +0800

    chore: 把提交新的反馈的链接修改至https://github.com/Hex-Dragon/PCL2/issues/new/choose，防止有小白看不懂英文或者不会反馈啥的，更好地反馈bug

--- Plain Craft Launcher 2/Modules/Base/ModBase.vb
+++ Plain Craft Launcher 2/Modules/Base/ModBase.vb
@@ -2762,7 +2762,7 @@ Retry:
         If ForceOpenLog OrElse (ShowMsgbox AndAlso MyMsgBox("若你在汇报一个 Bug，请点击 打开文件夹 按钮，并上传 Log(1~5).txt 中包含错误信息的文件。" & vbCrLf & "游戏崩溃一般与启动器无关，请不要因为游戏崩溃而提交反馈。", "反馈提交提醒", "打开文件夹", "不需要") = 1) Then
             OpenExplorer("""" & Path & "PCL\""")
         End If
-        OpenWebsite("https://github.com/Hex-Dragon/PCL2/issues/")
+        OpenWebsite("https://github.com/Hex-Dragon/PCL2/issues/new/choose")
     End Sub
     Public Function CanFeedback(ShowHint As Boolean) As Boolean
         If False.Equals(PageSetupSystem.IsLauncherNewest) Then
