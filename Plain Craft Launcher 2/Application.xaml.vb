23f00c4f212b596d35762af173dd30c6676ecf9a
ZeroWolf <z@l0.ink>
2024-11-25 23:32:46 +0800

    在下载文件夹中运行时给个提示

--- Plain Craft Launcher 2/Application.xaml.vb
+++ Plain Craft Launcher 2/Application.xaml.vb
@@ -116,6 +116,9 @@ WaitRetry:
             If Path.Contains(IO.Path.GetTempPath()) OrElse Path.Contains("AppData\Local\Temp\") Then
                 MyMsgBox("请将 PCL 从压缩文件中解压，或是更换文件夹后再继续使用！" & vbCrLf & "程序目前在临时文件夹中运行，设置、游戏存档等可能无法保存，且部分功能将无法使用。", "环境警告", "我知道了", IsWarn:=True)
             End If
+            If currentDirectory.ToLower().Contains("download") OrElse currentDirectory.Contains("下载") Then
+                MyMsgBox("不建议在下载文件夹中运行 PCL！" & vbCrLf & "PCL 目前似乎在您的下载文件夹中运行，这虽然大概率不会影响运行，但是您下次可能会找不到 PCL 在哪，建议您尽快将 PCL 复制到别处或单独的文件夹中运行", "环境警告", "我知道了", IsWarn:=True)
+            End If
             If Is32BitSystem Then
                 MyMsgBox("PCL 和新版 Minecraft 均不再支持 32 位系统，部分功能将无法使用。" & vbCrLf & "非常建议重装为 64 位系统后再进行游戏！", "环境警告", "我知道了", IsWarn:=True)
             End If
