0c5af68cae570d388592e994a49f6b3b922b94eb
tangge233 <tangge233@qq.com>
2025-01-02 23:45:04 +0800

    feat: 完成端口查找

--- Plain Craft Launcher 2/Modules/ModLink.vb
+++ Plain Craft Launcher 2/Modules/ModLink.vb
@@ -1,7 +1,72 @@
-﻿Imports System.Text.RegularExpressions
+﻿Imports System.Runtime.InteropServices
+Imports System.Text.RegularExpressions
 Imports Open.Nat
+Imports System.Net
 
 Public Class ModLink
+
+
+    Public Class PortFinder
+        ' 定义需要的结构和常量
+        <StructLayout(LayoutKind.Sequential)>
+        Public Structure MIB_TCPROW_OWNER_PID
+            Public dwState As Integer
+            Public dwLocalAddr As Integer
+            Public dwLocalPort As Integer
+            Public dwRemoteAddr As Integer
+            Public dwRemotePort As Integer
+            Public dwOwningPid As Integer
+        End Structure
+
+        <DllImport("iphlpapi.dll", SetLastError:=True)>
+        Public Shared Function GetExtendedTcpTable(
+        ByVal pTcpTable As IntPtr,
+        ByRef dwOutBufLen As Integer,
+        ByVal bOrder As Boolean,
+        ByVal ulAf As Integer,
+        ByVal TableClass As Integer,
+        ByVal reserved As Integer) As Integer
+        End Function
+
+        Public Shared Function GetProcessPort(ByVal dwProcessId As Integer) As List(Of Integer)
+            Dim ports As New List(Of Integer)()
+            Dim tcpTable As IntPtr = IntPtr.Zero
+            Dim dwSize As Integer = 0
+            Dim dwRetVal As Integer
+
+            If dwProcessId = 0 Then
+                Return ports
+            End If
+
+            dwRetVal = GetExtendedTcpTable(IntPtr.Zero, dwSize, True, 2, 5, 0)
+            If dwRetVal <> 0 AndAlso dwRetVal <> 122 Then ' 122 表示缓冲区不足
+                Return ports
+            End If
+
+            tcpTable = Marshal.AllocHGlobal(dwSize)
+            Try
+                If GetExtendedTcpTable(tcpTable, dwSize, True, 2, 5, 0) <> 0 Then
+                    Return ports
+                End If
+
+                Dim tablePtr As IntPtr = tcpTable
+                Dim dwNumEntries As Integer = Marshal.ReadInt32(tablePtr)
+                tablePtr = IntPtr.Add(tablePtr, 4)
+
+                For i As Integer = 0 To dwNumEntries - 1
+                    Dim row As MIB_TCPROW_OWNER_PID = Marshal.PtrToStructure(Of MIB_TCPROW_OWNER_PID)(tablePtr)
+                    If row.dwOwningPid = dwProcessId Then
+                        ports.Add(row.dwLocalPort >> 8 Or (row.dwLocalPort And &HFF) << 8) ' 转换端口号
+                    End If
+                    tablePtr = IntPtr.Add(tablePtr, Marshal.SizeOf(Of MIB_TCPROW_OWNER_PID)())
+                Next
+            Finally
+                Marshal.FreeHGlobal(tcpTable)
+            End Try
+
+            Return ports
+        End Function
+    End Class
 
 #Region "UPnP 映射"
     Public Shared Async Sub StartUPnPRequest(Optional LocalPort As Integer = 25565, Optional PublicPort As Integer = 10240)
@@ -9,7 +74,7 @@ Public Class ModLink
         Dim cts = New CancellationTokenSource(10000)
         Dim UPnPDevice = Await UPnPDiscoverer.DiscoverDeviceAsync(PortMapper.Upnp, cts)
 
-        Await UPnPDevice.CreatePortMapAsync(New Mapping(Protocol.Tcp, 10140, 10240, "PCL2 Link Lobby"))
+        Await UPnPDevice.CreatePortMapAsync(New Mapping(Protocol.Tcp, LocalPort, PublicPort, "PCL2 Link Lobby"))
         Hint("UPnP 映射已创建")
     End Sub
 #End Region
@@ -17,48 +82,36 @@ Public Class ModLink
 #Region "Minecraft 实例探测"
     Public Shared Sub MCInstanceFinding()
         'Java 进程 PID 查询
-        'Dim PIDLookupResult As String() = Nothing
-        'Dim JavaNames As String() = Nothing
-        'JavaNames.Append("java.exe")
-        'JavaNames.Append("javaw.exe")
+        Dim PIDLookupResult As New List(Of String)
+        Dim JavaNames As New List(Of String)
+        JavaNames.Add("java")
+        JavaNames.Add("javaw")
 
-        'For Each java In JavaNames
-        '    Dim JavaProcesses As Process() = Process.GetProcessesByName(java)
-        '    If JavaProcesses Is Nothing OrElse JavaProcesses.Length <= 0 Then
-        '        Continue For
-        '    Else
-        '        For Each p In JavaProcesses
-        '            Log("[MCDetect] 检测到 Java 进程，PID: " + p.Id.ToString())
-        '            PIDLookupResult.Append(p.Id.ToString())
-        '        Next
-        '    End If
-        'Next
+        For Each java In JavaNames
+            Dim JavaProcesses As Process() = Process.GetProcessesByName(java)
+            Log($"[MCDetect] 找到 {java} 进程 {JavaProcesses.Length} 个")
 
-        '通过 PID 查询对应进程监听的端口号
-        'Dim Pro As Process = New Process
+            If JavaProcesses Is Nothing OrElse JavaProcesses.Length = 0 Then
+                Continue For
+            Else
+                For Each p In JavaProcesses
+                    Log("[MCDetect] 检测到 Java 进程，PID: " + p.Id.ToString())
+                    PIDLookupResult.Add(p.Id.ToString())
+                Next
+            End If
+        Next
 
-        'Try
-        '    Dim NetStatOutput As String = ShellAndGetOutput("netstat", "-ano")
-        '    'Log(NetStatOutput)
-        '    Dim reg As Regex = New Regex("\\s+", RegexOptions.Compiled)
-        '    Dim line As String = Nothing
-        '    Dim line1 As String = Nothing
-        '    Dim reader As StringReader = New StringReader(NetStatOutput)
-        '    While Not line = reader.ReadLine()
-        '        line1 = reader.ReadLine().Trim()
+        Try
+            If Not PIDLookupResult.Any Then Exit Sub
+            Dim ports = PortFinder.GetProcessPort(Integer.Parse(PIDLookupResult.First))
+            Log($"[MCDetect] 获取到端口数量 {ports.Count}")
+            For Each port In ports
+                Hint($"找到端口：{port}")
+            Next
 
-        '        If Not line.StartsWithF("TCP") AndAlso Not line.StartsWithF("UDP") Then
-        '            Continue While
-        '        End If
-
-        '        line = reg.Replace(line, ",")
-        '        Log(line)
-        '        Dim soc As String = line.Split(",")(1)
-
-        '    End While
-        'Catch ex As Exception
-
-        'End Try
+        Catch ex As Exception
+            Log(ex, "[MCDetect] 获取端口信息错误", LogLevel.Debug)
+        End Try
 
     End Sub
 
