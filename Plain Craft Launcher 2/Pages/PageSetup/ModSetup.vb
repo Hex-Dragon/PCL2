f213e8d8718a569e3b3466b884de535a11a686c3
Pigeon0v0 <PigeonA0v0@outlook.com>
2025-01-23 14:39:39 +0800

    feat: 识别剪贴板资源并提示跳转

--- Plain Craft Launcher 2/Pages/PageSetup/ModSetup.vb
+++ Plain Craft Launcher 2/Pages/PageSetup/ModSetup.vb
@@ -116,6 +116,7 @@
         {"ToolDownloadVersion", New SetupEntry(0, Source:=SetupSource.Registry)},
         {"ToolDownloadTranslate", New SetupEntry(0, Source:=SetupSource.Registry)},
         {"ToolDownloadIgnoreQuilt", New SetupEntry(True, Source:=SetupSource.Registry)},
+        {"ToolDownloadClipboard", New SetupEntry(False, Source:=SetupSource.Registry)},
         {"ToolDownloadCert", New SetupEntry(False, Source:=SetupSource.Registry)},
         {"ToolDownloadMod", New SetupEntry(1, Source:=SetupSource.Registry)},
         {"ToolModLocalNameStyle", New SetupEntry(0, Source:=SetupSource.Registry)},
