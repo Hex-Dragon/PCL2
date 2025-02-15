730344230d2657ba9043938b918652c1b3488e67
Pigeon0v0 <60414767+Pigeon0v0@users.noreply.github.com>
2025-02-15 15:46:10 +0800

    Slow Ring 2.10.3 修改了关于社区版提示的表述

--- Plain Craft Launcher 2/FormMain.xaml.vb
+++ Plain Craft Launcher 2/FormMain.xaml.vb
@@ -12,6 +12,15 @@ Public Class FormMain
         Dim FeatureList As New List(Of KeyValuePair(Of Integer, String))
         '统计更新日志条目
 #If RELEASE Then
+        If LastVersion < 360 Then '2.10.3
+            FeatureList.Add(New KeyValuePair(Of Integer, String)(5, "支持多收藏夹，允许批量下载和分享"))
+            FeatureList.Add(New KeyValuePair(Of Integer, String)(4, "添加了更新通道机制"))
+            FeatureList.Add(New KeyValuePair(Of Integer, String)(4, "添加了国内本体更新和公告服务器，感谢 @pysio2007"))
+            FeatureList.Add(New KeyValuePair(Of Integer, String)(3, "更改 Java Wrapper 启用机制"))
+            FeatureList.Add(New KeyValuePair(Of Integer, String)(3, "修复 Authlib 验证可能失败的问题"))
+            FeatureCount += 5
+            BugCount += 7
+        End If
         If LastVersion < 357 Then
             FeatureList.Add(New KeyValuePair(Of Integer, String)(1, "修复第三方登录无效会话问题"))
             BugCount += 1
