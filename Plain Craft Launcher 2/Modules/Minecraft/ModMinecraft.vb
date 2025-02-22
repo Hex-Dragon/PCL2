ddd3df009d0d460c7c587d9939b6bc1b8aadaeb3
3gf8jv4dv <3gf8jv4dv@gmail.com>
2025-02-22 17:28:48 +0800

    fix: treat cleanroom instances as installable mods
    resolves https://github.com/PCL-Community/PCL2-CE/issues/314

--- Plain Craft Launcher 2/Modules/Minecraft/ModMinecraft.vb
+++ Plain Craft Launcher 2/Modules/Minecraft/ModMinecraft.vb
@@ -290,7 +290,7 @@ Public Module ModMinecraft
         Public ReadOnly Property Modable As Boolean
             Get
                 If Not IsLoaded Then Load()
-                Return Version.HasFabric OrElse Version.HasQuilt OrElse Version.HasForge OrElse Version.HasLiteLoader OrElse Version.HasNeoForge OrElse
+                Return Version.HasFabric OrElse Version.HasQuilt OrElse Version.HasForge OrElse Version.HasLiteLoader OrElse Version.HasNeoForge OrElse Version.HasCleanroom OrElse
                     DisplayType = McVersionCardType.API '#223
             End Get
         End Property
