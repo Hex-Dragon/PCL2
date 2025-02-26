9d9b4b89d13b633dbdfc08fc71a5a770268a875c
薄奚梦灵 <188181427+shimoranla@users.noreply.github.com>
2025-02-26 20:48:59 +0800

    Chore: Modify the text of the logging
    意义不明的文本+社区版没有反馈主题

--- Plain Craft Launcher 2/Application.xaml.vb
+++ Plain Craft Launcher 2/Application.xaml.vb
@@ -106,7 +106,7 @@ WaitRetry:
             LogStart()
             '添加日志
             Log($"[Start] 程序版本：{VersionBaseName} ({VersionBranchName}, {VersionCode}{If(CommitHash = "", "", $"，#{CommitHash}")})")
-            Log($"[Start] 识别码：{UniqueAddress}{If(ThemeCheckOne(9), "，已解锁反馈主题", "")}")
+            Log($"[Start] 识别码：{UniqueAddress}{If(ThemeCheckOne(9), "", "")}")
             Log($"[Start] 程序路径：{PathWithName}")
             Log($"[Start] 系统版本：{Environment.OSVersion.Version}, 架构：{Runtime.InteropServices.RuntimeInformation.OSArchitecture}")
             Log($"[Start] 系统编码：{Encoding.Default.HeaderName} ({Encoding.Default.CodePage}, GBK={IsGBKEncoding})")
