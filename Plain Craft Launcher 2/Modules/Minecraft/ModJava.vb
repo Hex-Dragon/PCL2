0c37755d730739ddb43f6ca16ad3fbe48ae5ed72
tangge233 <tangge233@qq.com>
2025-03-01 20:31:00 +0800

    feat(Java): 允许全局禁用 Java

--- Plain Craft Launcher 2/Modules/Minecraft/ModJava.vb
+++ Plain Craft Launcher 2/Modules/Minecraft/ModJava.vb
@@ -33,6 +33,10 @@
         ''' 是否为用户手动导入的 Java。
         ''' </summary>
         Public IsUserImport As Boolean
+        ''' <summary>
+        ''' 是否使用此 Java
+        ''' </summary>
+        Public IsEnabled As Boolean = True
 
         '版本信息
         ''' <summary>
@@ -68,10 +72,19 @@
 
         '序列化
         Public Function ToJson() As JObject
-            Return New JObject({New JProperty("Path", PathFolder), New JProperty("VersionString", Version.ToString), New JProperty("IsJre", IsJre), New JProperty("Is64Bit", Is64Bit), New JProperty("IsUserImport", IsUserImport)})
+            Return New JObject({New JProperty("Path", PathFolder),
+                               New JProperty("VersionString", Version.ToString),
+                               New JProperty("IsJre", IsJre),
+                               New JProperty("Is64Bit", Is64Bit),
+                               New JProperty("IsUserImport", IsUserImport),
+                               New JProperty("IsEnable", IsEnabled)})
         End Function
         Public Shared Function FromJson(Data As JObject) As JavaEntry
-            Return New JavaEntry(Data("Path"), Data("IsUserImport")) With {.Version = New Version(Data("VersionString")), .IsJre = Data("IsJre"), .Is64Bit = Data("Is64Bit")}
+            Return New JavaEntry(Data("Path"),
+                                 Data("IsUserImport")) With {.Version = New Version(Data("VersionString")),
+                                 .IsJre = Data("IsJre"),
+                                 .Is64Bit = Data("Is64Bit"),
+                                 .IsEnabled = If(Data("IsEnable"), True)}
         End Function
         ''' <summary>
         ''' 转化为用户友好的字符串输出。
@@ -297,6 +310,9 @@ RetryGet:
             Dim AllJavaList As New List(Of JavaEntry)
             AllJavaList.AddRange(TargetJavaList)
             AllJavaList.AddRange(JavaList)
+
+            '禁用用户不希望使用的 Java
+            AllJavaList = AllJavaList.Where(Function(i) i.IsEnabled).ToList()
 
             '根据选定条件进行过滤
             For Each Java In AllJavaList
@@ -583,7 +599,7 @@ NoUserJava:
                     If Entry.IsUserImport Then JavaPreList(Entry.PathFolder) = True
                 Next
             Catch ex As Exception
-                Log(ex, "Java 列表已损坏", LogLevel.Feedback)
+                Log(ex, "Java 列表已损坏，无法获取用户导入的 Java", LogLevel.Feedback)
                 Setup.Set("LaunchArgumentJavaAll", "[]")
             End Try
 
@@ -595,6 +611,20 @@ NoUserJava:
                 NewJavaList.Add(New JavaEntry(Entry.Key, Entry.Value))
             Next
             NewJavaList = Sort(JavaCheckList(NewJavaList), AddressOf JavaSorter)
+
+#Region "同步原有的启用信息"
+            Try
+                For Each JavaJsonObject In GetJson(ImportedJava)
+                    Dim Target = NewJavaList.Find(Function(j) j.PathFolder = JavaEntry.FromJson(JavaJsonObject).PathFolder)
+                    If Target IsNot Nothing Then
+                        Target.IsEnabled = JavaEntry.FromJson(JavaJsonObject).IsEnabled
+                    End If
+                Next
+            Catch ex As Exception
+                Log(ex, "Java 列表已损坏了，无法获取原有 Java 启用情况", LogLevel.Feedback)
+                Setup.Set("LaunchArgumentJavaAll", "[]")
+            End Try
+#End Region
 
             '修改设置项
             Dim AllList As New JArray
