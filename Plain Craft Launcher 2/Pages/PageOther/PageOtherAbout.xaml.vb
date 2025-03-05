afea0b41ad97789d528e5593f4652d0aaae96b0f
tangge233 <tangge233@qq.com>
2025-03-05 14:36:11 +0800

    chore(ClickLogo): 加个……彩蛋？

--- Plain Craft Launcher 2/Pages/PageOther/PageOtherAbout.xaml.vb
+++ Plain Craft Launcher 2/Pages/PageOther/PageOtherAbout.xaml.vb
@@ -21,4 +21,59 @@
         OpenWebsite("https://www.mcmod.cn")
     End Sub
 
+
+    Private ClickCount As Integer = 0
+
+    Private Sub ImgPCLLogo_Click(sender As Object, e As MouseButtonEventArgs) Handles ImgPCLLogo.MouseLeftButtonDown
+        If ClickCount < 200 Then
+            ClickCount += 1
+            Select Case ClickCount
+                Case 5
+                    Hint("点这个很好玩么……")
+                Case 15
+                    Hint("还点？")
+                Case 25
+                    Select Case MyMsgBox("你现在是不是超无聊的？", "咕咕咕？", Button1:="是的", Button2:="并不是")
+                        Case 2
+                            Hint("那你还点啥……真是搞不懂。")
+                    End Select
+                Case 50
+                    Hint("嗯，加油吧，嗯……")
+                Case 75
+                    Hint("隐藏主题 混乱黄 已……嗯不对，这是 PCL2，应该没有这玩意……")
+                Case 95
+                    Hint("你咋还这么无聊啊？")
+                Case 130
+                    Hint("后面什么都没有了哦！")
+                Case 155
+                    Select Case MyMsgBox("你真的不累么？", "温馨提示", "累死了", "真的不累")
+                        Case 1
+                            Hint("那你就别点了喂……后面真的真的真的什么都没有了！")
+                        Case 2
+                            Select Case MyMsgBox("你真的真的不累么？", "超温馨的温馨提示", "累死了", "真的真的不累")
+                                Case 1
+                                    Hint("那你就别点了喂……后面真的真的真的什么都没有了！")
+                                Case 2
+                                    Select Case MyMsgBox("你真的真的真的不累么？", "超超超温馨的温馨提示", "累死了", "真的真的真的不累")
+                                        Case 1
+                                            Hint("那你就别点了喂……后面真的真的真的什么都没有了！")
+                                        Case 2
+                                            Hint("好吧……不过后面是真的啥也没了，不用点了真的。")
+                                    End Select
+                            End Select
+                    End Select
+                Case 200
+                    Hint("还点，还点就不让你点了……")
+                    ImgPCLLogo.IsHitTestVisible = False
+                    Exit Sub
+            End Select
+            AniStart({
+                 AaTranslateX(sender, 1, Time:=0),
+                 AaTranslateY(sender, 1, Time:=0),
+                 AaTranslateX(sender, -1, Time:=0, Delay:=100),
+                 AaTranslateY(sender, -1, Time:=0, Delay:=100)
+                 })
+        End If
+    End Sub
+
 End Class
