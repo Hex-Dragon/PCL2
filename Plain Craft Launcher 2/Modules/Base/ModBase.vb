b62b5c75a8deb3b3797f9bbd5ef421b69ff486f2
tangge233 <tangge233@qq.com>
2025-03-10 23:05:45 +0800

    fix(Update): 判断是否需要更新逻辑有误

--- Plain Craft Launcher 2/Modules/Base/ModBase.vb
+++ Plain Craft Launcher 2/Modules/Base/ModBase.vb
@@ -3036,7 +3036,7 @@ Retry:
         OpenWebsite("https://github.com/PCL-Community/PCL2-CE/issues/")
     End Sub
     Public Function CanFeedback(ShowHint As Boolean) As Boolean
-        If LatestVersion <> VersionBaseName Then
+        If LatestVersionCode > VersionCode Then
             If ShowHint Then
                 If MyMsgBox($"你的 PCL 不是最新版，因此无法提交反馈。{vbCrLf}请在更新后，确认该问题在最新版中依然存在，然后再提交反馈。", "无法提交反馈", "更新", "取消") = 1 Then
                     UpdateCheckByButton()
