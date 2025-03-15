c03f731d5cca87f587ce299dfb44a24340df2097
tangge233 <tangge233@qq.com>
2025-03-15 11:12:31 +0800

    release(sr): 2.10.4 363

--- Plain Craft Launcher 2/FormMain.xaml.vb
+++ Plain Craft Launcher 2/FormMain.xaml.vb
@@ -12,6 +12,11 @@ Public Class FormMain
         Dim FeatureList As New List(Of KeyValuePair(Of Integer, String))
         '统计更新日志条目
 #If RELEASE Then
+        If LatestVersion < 363 Then '2.10.4
+            FeatureList.Add(New KeyValuePair(Of Integer, String)(5, "统一了模组、资源包和光影的管理界面"))
+            FeatureList.Add(New KeyValuePair(Of Integer, String)(3, "修复了未使用 Java 高性能选项的问题"))
+            FeatureList.Add(New KeyValuePair(Of Integer, String)(3, "修复了实时日志部分颜色不正确的问题"))
+        End If
         If LastVersion < 360 Then '2.10.3
             FeatureList.Add(New KeyValuePair(Of Integer, String)(5, "支持多收藏夹，允许批量下载和分享"))
             FeatureList.Add(New KeyValuePair(Of Integer, String)(4, "添加了更新通道机制"))
