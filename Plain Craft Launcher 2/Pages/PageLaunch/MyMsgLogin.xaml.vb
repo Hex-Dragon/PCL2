de7dffefe8fcb4fc62879471ca9d512e88771465
薄奚梦灵 <boximengling@outlook.com>
2025-04-23 20:28:24 +0800

    style(MyMsgLogin):统一格式
    Co-authored-by: Michaelshenmo <121526475+Michaelshenmo@users.noreply.github.com>

--- Plain Craft Launcher 2/Pages/PageLaunch/MyMsgLogin.xaml.vb
+++ Plain Craft Launcher 2/Pages/PageLaunch/MyMsgLogin.xaml.vb
@@ -125,8 +125,8 @@
                 ElseIf ex.Message.Contains("expired_token") Then
                     Finished(New Exception("$登录用时太长啦，重新试试吧！"))
                     Return
-                ElseIf ex.Message.Contains("abuse") Then
-                    Finished(New Exception("该账号已被封禁，无法登录"))
+                ElseIf ex.Message.ContainsF("abuse mode") Then
+                    Finished(New Exception("该账号似乎已被微软封禁，无法登录。"))
                 ElseIf ex.Message.Contains("AADSTS70000") Then '可能不能判 “invalid_grant”，见 #269
                     Finished(New RestartException)
                     Return
