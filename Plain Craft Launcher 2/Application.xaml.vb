9cb8f9d56ee4f24f617a78c4d0fd5842270c69c0
wyc-26 <wuyuncheng26@foxmail.com>
2025-05-02 21:17:20 +0800

    chore: 测试代码忘删了

--- Plain Craft Launcher 2/Application.xaml.vb
+++ Plain Craft Launcher 2/Application.xaml.vb
@@ -143,7 +143,6 @@ WaitRetry:
             ServicePointManager.Expect100Continue = True
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 Or SecurityProtocolType.Tls Or SecurityProtocolType.Tls11 Or SecurityProtocolType.Tls12
             ServicePointManager.DefaultConnectionLimit = 1024
-            'WebRequest.DefaultWebProxy = GetProxy()
             '设置字体
             Dim TargetFont As String = Setup.Get("UiFont")
             If Not String.IsNullOrEmpty(TargetFont) Then
