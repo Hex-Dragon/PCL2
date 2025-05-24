3f16679e38906278cde58a9c0567ceb55e85c051
tangge233 <tangge233@qq.com>
2025-05-25 00:06:20 +0800

    bump: 2.11.2-beta.1

--- Plain Craft Launcher 2/Modules/Base/ModBase.vb
+++ Plain Craft Launcher 2/Modules/Base/ModBase.vb
@@ -15,7 +15,7 @@ Public Module ModBase
 #Region "声明"
 
     '下列版本信息由更新器自动修改
-    Public Const VersionBaseName As String = "2.11.1" '不含分支前缀的显示用版本名
+    Public Const VersionBaseName As String = "2.11.2-beta.1" '不含分支前缀的显示用版本名
     Public Const VersionStandardCode As String = "2.11.1." & VersionBranchCode '标准格式的四段式版本号
     Public Const CommitHash As String = "native" 'Commit Hash，由 GitHub Workflow 自动替换
     Public CommitHashShort As String = If(CommitHash = "native", "native", CommitHash.Substring(0, 7)) 'Commit Hash，取前 7 位
