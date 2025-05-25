f21747f73f8947772ffd0ed1cecba0772a6be692
薄奚梦灵 <boximengling@outlook.com>
2025-05-25 13:41:01 +0800

    revert

--- Plain Craft Launcher 2/Pages/PageDownload/PageDownloadClient.xaml.vb
+++ Plain Craft Launcher 2/Pages/PageDownload/PageDownloadClient.xaml.vb
@@ -45,7 +45,7 @@
                             Case Else '4/1 自动视作愚人节版
                                 Dim ReleaseDate = Version("releaseTime").Value(Of Date).ToUniversalTime().AddHours(2)
                                 If ReleaseDate.Month = 4 AndAlso ReleaseDate.Day = 1 Then
-                                    Version.Add("lore", GetMcFoolName(Version("id")))
+
                                     Type = "愚人节版"
                                     Version("type") = "special"
                                 End If
