9f34ffc39fdb64d61f633e0dfcf4e67ae6f233e9
tangge233 <tangge233@qq.com>
2025-06-06 13:48:40 +0800

    chore: 漏了一处更改

--- Plain Craft Launcher 2/Modules/Minecraft/ModDownload.vb
+++ Plain Craft Launcher 2/Modules/Minecraft/ModDownload.vb
@@ -1413,8 +1413,6 @@
     ''' </summary>
     Public Function DlModRequest(Url As String, Method As String, Data As String, ContentType As String) As String
         Dim Urls As New List(Of KeyValuePair(Of String, Integer))
-        Urls.Add(New KeyValuePair(Of String, Integer)(Url, 5))
-        Urls.Add(New KeyValuePair(Of String, Integer)(Url, 20))
         Dim McimUrl As String = DlSourceModGet(Url)
         If McimUrl <> Url Then
             Select Case Setup.Get("ToolDownloadMod")
