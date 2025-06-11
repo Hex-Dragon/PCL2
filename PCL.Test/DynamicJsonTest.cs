ddd5d2c9506c46029cb56a2c2ea91b3d6ff31ba7
任天天 <ruattqwq@outlook.com>
2025-06-11 18:33:58 +0800

    test: 添加 HTTP 服务端和 JSON 动态序列化测试

---
+++ PCL.Test/DynamicJsonTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Dynamic;
+using System.Text.Json;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PCL.Core.Utils;
+
+namespace PCL.Test
+{
+    [TestClass]
+    public class DynamicJsonTest
+    {
+        [TestMethod]
+        public void TestDynamicJson()
+        {
+            dynamic obj = new { a = 123, b = new { c = this, d = "text" } };
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true, 
+                Converters = { new ExpandoObjectConverter() },
+                AllowTrailingCommas = true
+            };
+            string json = JsonSerializer.Serialize(obj, options);
+            Console.WriteLine(json);
+            dynamic obj2 = JsonSerializer.Deserialize<ExpandoObject>(json, options);
+            Console.WriteLine(obj2.ToString());
+        }
+    }
+}
---
+++ PCL.Test/WebServerTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PCL.Core.Service;
+
+namespace PCL.Test;
+
+[TestClass]
+public class WebServerTest
+{
+    /// <summary>
+    /// Please run the following command:
+    /// <code>
+    /// curl http://localhost:8080/test/foo
+    /// curl http://localhost:8080/json
+    /// curl -I http://localhost:8080/any/path
+    /// curl -I http://localhost:8080
+    /// </code>
+    /// </summary>
+    [TestMethod]
+    public async Task TestRoutedWebServer()
+    {
+        Console.WriteLine("Starting web server with default listen (127.0.0.1:8080)...");
+        var server = new RoutedWebServer();
+        
+        server.Route("/test", (path, _) => RoutedResponse.Text(path));
+        Console.WriteLine("Test(/test): 200 OK (path relative to /test)");
+        await server.StartResponseOnce();
+        
+        dynamic obj = new { a = 123, b = new { c = this, d = "text" } };
+        server.Route("/json", () => RoutedResponse.Json(obj));
+        Console.WriteLine("Test(/json): 200 OK (request JSON)");
+        await server.StartResponseOnce();
+        
+        Console.WriteLine("Test(/any/path): 404 Not Found");
+        await server.StartResponseOnce();
+        
+        server.Route("/", () => RoutedResponse.NoContent);
+        Console.WriteLine("Test(/): 204 No Content");
+        await server.StartResponseOnce();
+        
+        server.Dispose();
+        Console.WriteLine("Test complete.");
+    }
+}
